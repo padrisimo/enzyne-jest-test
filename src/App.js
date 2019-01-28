97b449f867053b74025da7f15dc2db9375fc8ca1
padrisimo <adrianfernandezchamizo@gmail.com>
2019-01-28 18:38:27 +0100

    Test Redux Connect Components with Enzyme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import logo from './logo.svg'
+import TodoList from './TodoList'
 import './App.css'
 
 class App extends Component {
@@ -22,17 +22,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-          <h3 className={this.state.mainColor}>Everyone is Welcome!</h3>
-        </header>
-        <p className="App-intro">Hello World</p>
-        <p className='button-state'>{this.state.on ? 'Yes!' : 'No!'}</p>
-        <button onClick={() => this.setState({on: true})}>Click</button>
-        <h2>{this.state.input}</h2>
-        <input onChange={(e) => this.setState({input: e.currentTarget.value})} type='text' />
-        <p className='lifeCycle'>{this.state.lifeCycle}</p>
+        <TodoList />
       </div>
     )
   }
