865eb79749224e3bd5a8bfe0642d7c1b80593d56
padrisimo <adrianfernandezchamizo@gmail.com>
2019-01-28 16:58:06 +0100

    Test React Component Props with Enzyme and Jest

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { configure, shallow } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App, { Link } from './App'
+import { configure, shallow, mount } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import toJson from 'enzyme-to-json'
 
-configure({ adapter: new Adapter() });
+configure({ adapter: new Adapter() })
 
-describe('<App />', () => {
-  it('should contain 1 p element and h1 text', () => {
+describe('<App /> shallow rendering', () => {
+  it('h1 contains correct text', () => {
     const wrapper = shallow(<App />)
-    expect(wrapper.find('p').length).toBe(1)
     expect(wrapper.find('h1').text()).toBe('Welcome to React')
   })
-})  
+  it('matches the snapshot', () => {
+    const tree = shallow(<App />)
+    expect(toJson(tree)).toMatchSnapshot()
+  })
+})
+
+describe('<App /> mount rendering', () => {
+  it('h1 contains correct text', () => {
+    const wrapper = mount(<App />)
+    expect(wrapper.find('h1').text()).toBe('Welcome to React')
+    wrapper.unmount()
+  })
+  it('matches the snapshot', () => {
+    const tree = mount(<App />)
+    expect(toJson(tree)).toMatchSnapshot()
+    tree.unmount()
+  })
+})
+
+describe('<Link />', () => {
+  it('link component accepts address prop', () => {
+    const wrapper = shallow(<Link address='www.google.com' />)
+    expect(wrapper.instance().props.address).toBe('www.google.com')
+  })
+  it('a tag node renders href correctly', () => {
+    const wrapper = shallow(<Link address='www.google.com' />)
+    expect(wrapper.props().href).toBe('www.google.com')
+  })
+  it('returns null with true hide prop', () => {
+    const wrapper = shallow(<Link hide={false} />)
+    expect(wrapper.find('a').length).toBe(1)
+    wrapper.setProps({ hide: true })
+    expect(wrapper.get(0)).toBeNull()
+  })
+})
