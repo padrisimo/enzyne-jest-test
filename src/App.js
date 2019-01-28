1539efe67776ebfd7a2e602365a20bee66fef98b
padrisimo <adrianfernandezchamizo@gmail.com>
2019-01-28 16:39:56 +0100

    Find Nodes from a Shallow Rendered Component

--- src/App.js
+++ src/App.js
@@ -4,25 +4,17 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
+    return(
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Welcome to React</h1>
         </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
 }
-
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+configure({ adapter: new Adapter() });
+
+describe('<App />', () => {
+  it('should contain 1 p element and h1 text', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('p').length).toBe(1)
+    expect(wrapper.find('h1').text()).toBe('Welcome to React')
+  })
+})  
